ad56cd1ce9af782e79594f0250764f5810d95c28
Marcello Cavazza <70125046@amcontratos.com.br>
2024-04-01 07:59:50 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿int[,] maze = new int[10, 50];
+
+Random rand = new();
+int linhaPartida = rand.Next(1, 9);
+maze[linhaPartida, 1] = 2;
+
+int linhaObjetivo = rand.Next(1, 9);
+maze[linhaObjetivo, 48] = 3;
+
+definedObstacles(maze, rand);
+showMaze(maze);
+await Task.Delay(1000);
+Console.WriteLine("Em Real time o robo montará o melhor caminho!");
+await Task.Delay(2000);
+Console.Clear();
+void showMaze(int[,] maze)
+{
+    for (int i = 0; i < maze.GetLength(0); i++)
+    {
+        for (int j = 0; j < maze.GetLength(1); j++)
+        {
+            bool isWall = i == 0 || j == 0 || i == maze.GetLength(0) - 1 || j == maze.GetLength(1) - 1;
+            if (maze[i, j] == 2)
+            {
+                Console.Write("I");
+            }
+            else if (maze[i, j] == 3)
+            {
+                Console.Write("F");
+            }
+            else if (maze[i, j] == -1 || isWall)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+void definedObstacles(int[,] maze, Random rand)
+{
+    int numObstaculos = 50;
+    for (int i = 0; i < maze.GetLength(0); i++)
+    {
+        for (int j = 0; j < maze.GetLength(1); j++)
+        {
+            bool isWall = i == 0 || j == 0 || i == maze.GetLength(0) - 1 || j == maze.GetLength(1) - 1;
+
+            if (isWall)
+            {
+                maze[i, j] = -1;
+            }
+            else if(maze[i, j] != 2 && maze[i, j] != 3 && maze[i, j] != -1)
+            {
+                maze[i, j] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < numObstaculos; i++)
+    {
+        int linha = rand.Next(1, 8);
+        int coluna = rand.Next(1, 48);
+        if (maze[linha, coluna] != 2 || maze[linha, coluna] != 3)
+        {
+            maze[linha, coluna] = -1;
+        }
+    }
+}
